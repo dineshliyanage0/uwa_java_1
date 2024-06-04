83d88bdf07d60e5af9341e14084bc027dd3f0d3e
Dinesh Liyanage <dinesh@dlad.io>
2024-06-04 21:13:49 +0530

    first commit

---
+++ Cat.java
@@ -0,0 +1,22 @@
+/**
+ * Cat
+ */
+public class Cat {
+
+    public void speak() {
+        if (false) {
+            System.out.println("im speaking");
+        }
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat(); // initialize
+        
+        cat.speak();
+        
+    }
+}
+
+
+
+// .java -- compile --> byte code --> interprit
